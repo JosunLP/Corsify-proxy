8f554bdf27f0514e048475ba3459e937af769439
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-08-03 20:00:15 +0200

    cleaning

--- framework.src/custom/plugin/GalleryBuilder/manifest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include 'class/Gallery.class.php';
-
-echo '<link rel="stylesheet" href="custom/plugin/gallerybuilder/content/css/style.css">';
--- framework.src/page/open/Home.page.php
+++ framework.src/page/open/Home.page.php
@@ -9,7 +9,7 @@ Master: null;
 
 <h2>Welcome to the Corsify Proxy</h2>
 <h4>This page will give you a ruff description, how to use the proxy API</h4>
-</br>
+<br />
 <p>
      First of all, this proxy is intended to be used only with endpoints that have permission from the owner. We are not responsible for actions against this premise.
      Further details can be found in our <a href="/termsofuse">terms of use</a>!
@@ -18,7 +18,7 @@ Master: null;
 <hr>
 <h3>Here we have an basic example of how we talk to the API</h3>
 <code>https://domain/API.php?apiMode=feed&feedMode=rss&dataUrl=https://josunlp.de/feed</code>
-<br/>
+<br />
 <div class="caption">
      <p>
           To talk with the proxy API we select the <b>apiMode</b> <code>feed</code>.
