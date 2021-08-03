1df397c0a0d43e10c18d1a54b2928de9340481aa
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-08-03 19:10:05 +0200

    adding brand

--- framework.src/page/view/footer.php
+++ framework.src/page/view/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
 	<p>
 		<?php echo "&copy;" . COPYRIGHT . " " . date("Y"); ?>
-		<a href="/Impressum"> Imprint</a>
+		<a href="https://josunlp.de/impressum"> Imprint</a>
 	</p>
 	<div class="status"></div>
 </footer>
