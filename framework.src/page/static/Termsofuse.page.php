33feb9bc9fdb509763cd3aef1b72379aae73ed11
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-08-04 15:50:30 +0200

    translate

--- framework.src/page/static/Termsofuse.page.php
+++ framework.src/page/static/Termsofuse.page.php
@@ -6,14 +6,4 @@ Title: true;
 */
 
 ?>
-<p>Wir übernehmen keine Haftung für die Zugriffe,
-    die über diesen Proxy stattfinden.
-    Der Proxy darf nur bei Endpunkten verwendet werden,
-    von welchen eine Genehmigung des Anbieters vorliegt.
-    Wir speichern KEINE Daten, es liegt nicht einmal eine angebundene Datenbank vor.
-    Wir setzen keine Cookies, außer den technisch notwendigen.
-    Wir arbeiten mit keinem Tracing Anbieter zusammen.
-    Bei missbrauch unseres Proxys bitten wir,
-    sich in erster Instanz an den Verursacher zu wenden.
-    Sollte dieses ohne Erfolg gekrönt sein,
-    können sie sich unter support@josunlp.de melden, damit wir eine Lösung finden.</p>
+<p>We do not assume any liability for the access that takes place via this proxy. The proxy may only be used with endpoints that have been approved by the provider. We do NOT save any data, there is not even a connected database. We do not set any cookies, except for those that are technically necessary. We do not work with any tracing provider. In the event of misuse of our proxy, we ask you to contact the person responsible in the first instance. If this is unsuccessful, you can contact support@josunlp.de so that we can find a solution.</p>
