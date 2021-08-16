645338976aab77219f15f91555da7e746c776893
JosunLP <jolun.swtor@gmail.com>
2021-08-16 12:12:26 +0200

    removing all frontend parts

--- framework.src/page/static/Termsofuse.page.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
-PAGEINFO
-Title: true;
-*/
-
-?>
-<p>We do not assume any liability for the access that takes place via this proxy. The proxy may only be used with endpoints that have been approved by the provider. We do NOT save any data, there is not even a connected database. We do not set any cookies, except for those that are technically necessary. We do not work with any tracing provider. In the event of misuse of our proxy, we ask you to contact the person responsible in the first instance. If this is unsuccessful, you can contact support@josunlp.de so that we can find a solution.</p>
